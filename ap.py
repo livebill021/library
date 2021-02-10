0a0c50190973dc0f61554f933426718b142da5b4
livebill021 <76692285+livebill021@users.noreply.github.com>
2021-02-10 17:44:38 +0600

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+from openpyxl import load_workbook
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+@app.route("/books/")
+def books():
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sayfa1"]
+
+    object_list = [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
+    return render_template("books.html", object_list=object_list)
+    # tales = [tale.value for tale in page["A"]][1:]
+    # tales = []
+    # for tale in page["A"][1:]:
+    #     tales.append(tale.value)
+
+    # authors = [author.value for author in page["B"]][1:]   
+
+    # html = """
+    #     <a href="/authors">Авторы</a>
+    #     <a href="/books">Книги</a>
+    #     <h1 style="color: red">Тут будет список книг:</h1>
+    # """
+
+    # for i in range(len(tales)):
+    #     html += f"<h2>{tales[i]} - {authors[i]}</h2>"
+
+    # return html
+
+
+@app.route("/authors/")
+def authors():
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sayfa1"]
+    authors = {author.value for author in page["B"][1:]}
+    return render_template(
+        "authors.html", authors=list(authors)
+    )
+
+
+@app.route("/add/", methods=["POST"])
+def add():
+    f = request.form
+    # print(f["author"], f["book"])
+    excel = load_workbook("tales.xlsx")
+    page = excel["Sayfa1"]
+    last = len(page["A"]) + 1
+    page[f"A{last}"] = f["book"]
+    page[f"B{last}"] = f["author"]
+    excel.save("tales.xlsx")
+    return "форма получена"
+
+
+@app.route("/book/<num>/") # 5
+def book(num): # 5
+    excel = load_workbook("tales.xlsx")
+    page = excel["Лист1"]
+    object_list = [[tale.value, tale.offset(column=1).value] for tale in page["A"][1:]]
+    obj = object_list[int(num)] # object_list[5]
+    return render_template("book.html", obj=obj)
